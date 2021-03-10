696080a03bac841921711ca931420bf87120533a
Jiwon Yoon <10348029+JiWon0502@users.noreply.github.com>
2021-03-11 01:19:36 +0900

    Add files via upload
    OOP(03) Lab01

---
+++ OOP(03) Lab1/Lab01/src/TwoNumbers.java
@@ -0,0 +1,8 @@
+/* 두 숫자를 출력하는 프로그램*/
+public class TwoNumbers {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =10, b = 20;
+		System.out.println("a = " + a + " b = " + b);
+	}
+}
---
+++ OOP(03) Lab1/Lab01/src/TwoNumbers1.java
@@ -0,0 +1,11 @@
+
+public class TwoNumbers1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =10, b = 20;
+		System.out.println(a + " " + b);
+		System.out.println(a + b);
+		System.out.println("a = " + a+ " b = " + b);
+		System.out.printf("%d", a);
+	}
+}
