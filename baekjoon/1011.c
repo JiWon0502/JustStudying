87396a06a9178c91dba743a16eafe04c4ffb6968
Jiwon Yoon <10348029+JiWon0502@users.noreply.github.com>
2021-03-28 23:54:57 +0900

    Add files via upload

--- baekjoon/1011.c
+++ baekjoon/1011.c
@@ -1,22 +1,23 @@
 #include<stdio.h>
 
-long long calc(long long int a){
-    long long n=0;
+long long int calc(long long int a){
+    long long int n=0;
     while(1){
-        n++;
-        if(n*(n+1)<a && a < (n+1)*(n+2))
+        if(n*(n+1) < a && a <= (n+1)*(n+2))
             break;
+        n++;
     }
+    n++;
     if(a<=(n*(n-1)+n)) return 2*n-1;
     else return 2*n;
 }
 
 int main(){
     int T;
-    long long x, y;
+    long long int x, y;
     scanf("%d", &T);
     for(int i = 0; i<T; i++){
-        scanf("&llu &llu", &x, &y);
-        printf("%llu", calc(y-x));
+        scanf("%lld %lld", &x, &y);
+        printf("%lld\n", calc(y-x));
     }
 }
