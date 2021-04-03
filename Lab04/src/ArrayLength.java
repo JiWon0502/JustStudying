94362e3772cc8ab8914db81f43c7f30f09e09ae1
Jiwon Yoon <10348029+JiWon0502@users.noreply.github.com>
2021-04-04 02:33:55 +0900

    Add files via upload

---
+++ Lab04/src/ArrayLength.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ArrayLength {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int Array[] = new int[4];
+		int sum = 0;
+		System.out.println(Array.length + "개의 정수를 입력하세요>> ");
+		for(int i = 0; i<Array.length; i++) {
+			Array[i] = s.nextInt();
+		}
+		for(int i = 0; i < Array.length; i++) {
+			sum+= Array[i];
+		}
+		System.out.println("평균: "+ sum/Array.length);
+		s.close();
+	}
+
+}
---
+++ Lab04/src/DoWhileEx.java
@@ -0,0 +1,13 @@
+
+public class DoWhileEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char c = 'a';
+		do {
+			System.out.print(c);
+			c = (char)(c+1);
+		}while(c<='z');
+	}
+
+}
