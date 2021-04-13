5fb982ef0a1ee54ece716a5c3582074ae6250b04
Jiwon Yoon <10348029+JiWon0502@users.noreply.github.com>
2021-04-13 23:16:15 +0900

    Add files via upload

---
+++ Lab05/src/BookEx.java
@@ -0,0 +1,12 @@
+
+public class BookEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book Prince = new Book("어린왕자", "생텍쥐페리");
+		Book loveStory = new Book("춘향전");
+		Book emptyBook = new Book();
+		loveStory.show();
+	}
+
+}
