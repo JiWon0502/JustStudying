2933158d53d0190fabec70a2590835a6d51e09c2
Jiwon Yoon <10348029+JiWon0502@users.noreply.github.com>
2021-05-03 23:47:47 +0900

    Add files via upload

--- baekjoon/19597.c
+++ baekjoon/19597.c
@@ -1,3 +1,21 @@
 #include<stdio.h>
+#include<string.h>
+
 int main(){
+    int T, N, ans[151] = {0,};
+    scanf("%d", &T);
+    while(T-->0){
+        char arr[150][20] = {0,};
+        scanf("%d", &N);
+        for(int i = 0; i<N; i++) scanf("%s", arr[i]);
+        for(int i = N-1; i>=0; i--){
+            if(strcmp(arr[i], arr[i-1])<0)
+                ans[i] = 1;
+            else ans[i] = 0;
+        }
+        if(strcmp(arr[0], arr[1])>0) ans[0] = 1;
+        else ans[0] = 0;
+        for(int i = 0; i<N; i++) printf("%d", ans[i]);
+        printf("\n");
+    }
 }
