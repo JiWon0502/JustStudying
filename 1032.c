f4fb5c2e8940d10338ed1d2470ad26aec73afd70
Jiwon Yoon <10348029+JiWon0502@users.noreply.github.com>
2021-05-10 23:44:37 +0900

    Add files via upload

---
+++ 1032.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n;
+    char arr[50][50];
+    scanf("%d", &n);
+    for(int i = 0; i < n;i++)
+        scanf("%s", &arr[i]);
+    for(int i = 0; i<n;i++){
+        for(int j = 0; j<n; j++){
+        }
+    }
+}
