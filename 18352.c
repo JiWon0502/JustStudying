f9e93d89ea3dc717395c172d699ce8f7ca6adf74
Jiwon Yoon <10348029+JiWon0502@users.noreply.github.com>
2021-05-15 15:23:43 +0900

    Add files via upload

--- 18352.c
+++ 18352.c
@@ -1,3 +1,17 @@
 #include<stdio.h>
+int que[3000001] = {0,};
+int visit[300001] = {0,};
 int main(){
+    int N, M, K, X;
+    int **arr;
+    arr = (int**) malloc(sizeof(int*) *N);
+    arr[0] = (int*) malloc (sizeof(int)*N*N);
+    for(int i = 1; i<height; i++) arr[i] = arr[i-1] + width;
+    scanf("%d %d %d %d", &N, &M, &K, &X);
+    for(int i = 0; i<M; i++) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        arr[a-1][b-1] = 1;
+    }
+    
 }
