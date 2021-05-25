8b4038c8ce2150e4a9a1a8509056677f5d79128e
Jiwon Yoon <10348029+JiWon0502@users.noreply.github.com>
2021-05-25 18:14:31 +0900

    Add files via upload

---
+++ Lab11/src/StringDemo.java
@@ -0,0 +1,12 @@
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String a  = "Often in Java programs you need to compare two objects to determine "+"if they are equal or not. ";
+		String b = "It turns out there are two different kinds of equality one can determine"+ " about objects in JAva, reference equality or logical equality.";
+		
+		String c  = a.concat(b);
+		
+	}
+
+}
