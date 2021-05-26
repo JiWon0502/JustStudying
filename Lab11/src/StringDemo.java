89733643b717f3f949b1c31ba3ac69990ad1a26d
Jiwon Yoon <10348029+JiWon0502@users.noreply.github.com>
2021-05-26 18:47:20 +0900

    Add files via upload

--- Lab11/src/StringDemo.java
+++ Lab11/src/StringDemo.java
@@ -6,7 +6,22 @@ public class StringDemo {
 		String b = "It turns out there are two different kinds of equality one can determine"+ " about objects in JAva, reference equality or logical equality.";
 		
 		String c  = a.concat(b);
+		String s[] = c.split(" ");
+		System.out.println("단어 개수 = "+ s.length);
+		for(int i = 0; i<s.length; i++)
+			System.out.println(s[i]);
 		
+		System.out.println();
+		c = c.replace("",",");
+		System.out.println(c);
+		
+		c = c.concat(a);
+		System.out.println(c);
+		
+		System.out.println("--------------");
+		s = c.split(",| ");
+		System.out.println("단어 개수 = "+s.length);
+		for(int i = 0; i<s.length; i++)
+			System.out.println(s[i]);
 	}
-
 }
