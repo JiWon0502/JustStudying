7186801e7024ade4b5f19ef4dab1786848ff6b93
Jiwon Yoon <10348029+JiWon0502@users.noreply.github.com>
2021-05-28 22:30:49 +0900

    Add files via upload

--- 5176.c
+++ 5176.c
@@ -1,3 +1,20 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main(){
+    int K,P,M;
+    scanf("%d", &K);
+    while(K-->0){
+        int ans = 0;
+        scanf("%d %d", &P, &M);
+        int* arr = (int*) malloc(sizeof(int) * M);
+        for(int i = 0; i<M; i++) arr[i]=0;
+        for(int i = 0; i<P;i++){
+            int pos;
+            scanf("%d", &pos);
+            if(arr[pos-1] != 0) ans++;
+            else arr[pos-1] = 1;
+        }
+        printf("%d\n",ans);
+        free(arr);
+    }
 }
