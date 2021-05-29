3e3e6574889d211aabe43e875a6883639d33c1b1
Jiwon Yoon <10348029+JiWon0502@users.noreply.github.com>
2021-05-29 23:49:36 +0900

    Add files via upload

--- 16237.c
+++ 16237.c
@@ -5,7 +5,24 @@ int main() {
     int ans = 0;
     for(int i = 1; i < 6; i++) scanf("%d", &num[i]);
     ans = num[3] + num[4] + num[5];
-    
-
-    printf("%d\n", ans);
+    if(num[2] > num[3]){
+        num[2] -= num[3];
+        if(num[1] > num[4]){
+            num[1]-=num[4];
+            ans+=(num[2]/2+num[2]%2);
+            num[1] -= (num[2]/2 + num[2]%2*3);
+            ans+=(num[1]+4)/5;
+        }
+        else{
+            ans+=(num[2]/2+num[2]%2);
+        }
+    }
+    else{
+        num[3]-=num[2];
+        if((num[3]*2+num[4])<num[1]){
+            num[1]-=(num[3]*2+num[4]);
+            ans+=(num[1]+4)/5;
+        }
+    }
+    printf("%d", ans);
 }
