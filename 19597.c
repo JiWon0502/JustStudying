5ae1ebd8fe0c6286862bbbf36169005998f2b896
Jiwon Yoon <10348029+JiWon0502@users.noreply.github.com>
2021-05-31 02:19:46 +0900

    Add files via upload

--- 19597.c
+++ 19597.c
@@ -4,19 +4,20 @@
 int main(){
     int T, N, ans[151] = {0,};
     scanf("%d", &T);
-    while(1){
+    while(T-->0){
         char arr[150][20] = {0,};
         scanf("%d", &N);
         for(int i = 0; i<N; i++) scanf("%s", arr[i]);
-        for(int i = N-1; i>=0; i--){
-            if(strcmp(arr[i], arr[i-1])<0)
-                ans[i] = 1;
-            else ans[i] = 0;
+        while(tgl != 0){
+            for(int i = N-1; i>=0; i--){
+                if(strcmp(arr[i], arr[i-1])<0)
+                    ans[i] = 1;
+                else ans[i] = 0;
+            }
         }
         if(strcmp(arr[0], arr[1])>0) ans[0] = 1;
         else ans[0] = 0;
         for(int i = 0; i<N; i++) printf("%d", ans[i]);
-        if(--T<0) break;
-        else printf("\n");
+        printf("\n");
     }
 }
