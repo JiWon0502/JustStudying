d2644ba4d8f733a0bde98cdd7e4f3bba757293f6
Jiwon Yoon <10348029+JiWon0502@users.noreply.github.com>
2021-06-03 14:19:46 +0900

    Add files via upload

---
+++ 1757.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int chk(){ //다음 달리기 시작 가능 시간을 리턴
+    
+    
+}
+
+int main(){
+    int N, M;
+    int d[10001] = {0,};
+    scanf("%d %d", &N, &M);
+    for(int i = 0; i<M; i++)
+        scanf("%d", d[i]);
+}
