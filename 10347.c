4e8c5ce2e97c7cb2071391e67085a8e707979d09
Jiwon Yoon <10348029+JiWon0502@users.noreply.github.com>
2021-06-04 23:08:41 +0900

    Add files via upload

---
+++ 10347.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+//ascii code A~Z =  65~90
+
+char encrypt(char c, int N){
+    c += N;
+    ;
+}
+
+int main(){
+    while(1){
+        int N;
+        char s[41];
+        scanf("%d %s", &N, s);
+        
+    }
+}
