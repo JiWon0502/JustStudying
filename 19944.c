09cb1f01af9767e586fb7b6eddf2486c79ee4cad
Jiwon Yoon <10348029+JiWon0502@users.noreply.github.com>
2021-06-14 23:38:05 +0900

    Add files via upload

---
+++ 19944.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int N, M;
+    scanf("%d %d", &N, &M);
+    if(M<=2) printf("NEWBIE!");
+    else if(M<=N) printf("OLDBIE!");
+    else printf("TLE!");
+    return 0;
+}
