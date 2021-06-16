efec429f39cd694ddb56f4cf38fb0716aedd9772
Jiwon Yoon <10348029+JiWon0502@users.noreply.github.com>
2021-06-16 11:07:08 +0900

    Add files via upload

---
+++ HW04/src/AddressNum.java
@@ -0,0 +1,9 @@
+//2071032_ComputerScienceEngineering_YoonJiWon
+public class AddressNum {
+	String address;
+	String phoneNumber;
+	public AddressNum(String address, String phoneNumber) {
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+	}
+}
